d74e899643ea3879e0698f3d3c141652f5f618aa
Riemsloh <olaf.lueg@t-online.de>
2025-06-03 20:49:03 +0200

    Initial Commit

---
+++ AuraCast/AuraCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraCastApp.swift
+//  AuraCast
+//
+//  Created by Olaf Lueg on 03.06.25.
+//
+
+import SwiftUI
+
+@main
+struct AuraCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuraCast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuraCast
+//
+//  Created by Olaf Lueg on 03.06.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
